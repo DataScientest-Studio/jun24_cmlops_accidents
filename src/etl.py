ddc0abb2e9aa88aae2f92e4911e9585e5d6f4e8e
Nico <nicolas.fenassile@gmail.com>
2024-10-14 17:37:19 +0200

    build_features integrated in ETL

--- src/etl.py
+++ src/etl.py
@@ -1,13 +1,13 @@
 from data.make_dataset import save_final_dataset, merge_datasets
-from features.build_features import load_final_dataset, transform_data
-from config import OUTPUT_FILE_PATH, PROCESSED_DATA_DIR
+from features.build_features import transform_data, build_model_features, select_variables_and_one_hot, save_test_train
+from config import PROCESSED_DATA_DIR
 import os
 
 def run_etl():
     """
     Exécute la pipeline ETL complète.
     """
-    # Assurez-vous que le dossier de sortie existe
+    # S'ssurer que le dossier de sortie existe
     os.makedirs(PROCESSED_DATA_DIR, exist_ok=True)
     
     # Étape 1: Extraire et fusionner les données
@@ -23,7 +23,7 @@ def run_etl():
 
     # Étape 3: Transformation des données
     print("Transformation des données...")
-    transformed_df = transform_data(df_final)  # Appel à la fonction de transformation
+    transformed_df = transform_data(df_final)
     print("Données transformées.")
 
     # Étape 4: Sauvegarder le DataFrame transformé dans data/processed
@@ -32,3 +32,24 @@ def run_etl():
     save_final_dataset(transformed_df, transformed_output_path)
     print(f"Le fichier transformé a été sauvegardé à {transformed_output_path}")
 
+    # Étape 5: Création des features du modèle
+    print("Création des features du modèle...")
+    features_df = build_model_features(transformed_df)
+    print("Features créées")
+
+    # Étape 6: Sauvegarder le DataFrame avec features dans data/processed
+    features_output_path = os.path.join(PROCESSED_DATA_DIR, 'df_with_features.csv')
+    print("Sauvegarde du DataFrame transformé...")
+    save_final_dataset(features_df, features_output_path)
+    print(f"Le fichier transformé a été sauvegardé à {features_output_path}")
+
+    # Étape 7: Sélection des variables, OneHotEncoding et Undersampling
+    print("Sélection des variables, OneHotEncoding et Undersampling...")
+    encoded_df = select_variables_and_one_hot(features_df)
+    print("Données encodées")
+
+    # Étape 8: Sauvegarder les dataframes train et test encodés
+    encoded_output_path = os.path.join(PROCESSED_DATA_DIR, 'df_encoded.csv')
+    print("Sauvegarde des dataframes encodés...")
+    save_final_dataset(encoded_df, encoded_output_path)
+    print(f"Les fichier encodés ont été sauvegardé à {encoded_output_path}")
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
-from config import PROCESSED_DATA_DIR, OUTPUT_FILE_PATH
+import os
 
-def load_final_dataset():
-    """
-    Charge le DataFrame final à partir du fichier CSV.
-    """
-    df = pd.read_csv(OUTPUT_FILE_PATH)  # Charger le df final depuis le chemin spécifié
-    return df
+from config import PROCESSED_DATA_DIR
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import train_test_split
 
 def homogenize_hour_format(row):
     """Homogénéise le format de l'heure dans la colonne 'hrmn'."""
@@ -207,20 +208,125 @@ def map_categories(df):
 
     return df
 
-
-def save_transformed_dataset(df, output_path):
+def build_model_features(df):
     """
-    Sauvegarde le DataFrame transformé dans le chemin spécifié.
+    Construit les catégories d'age, d'heures et de jour de la semaine, puis les encode.
     """
-    df.to_csv(output_path, index=False)
-    print(f"Le fichier transformé a été sauvegardé à {output_path}")
 
-def main():
-    # Chargement du DataFrame final
-    df_final = load_final_dataset()
+    # 1. Transformation de la variable horaire 'hrmn'
+    # Création de la variable 'hour_cat' en fonction des heures
+    df['hour_cat'] = pd.to_datetime(df['hrmn'], format='%H:%M').dt.hour
 
-    # Transformation des données
-    transformed_df = transform_data(df_final)
+    # Réduire les catégories horaires en 3 groupes
+    def reduce_categories_hour(x):
+        if x in range(9, 17):
+            return 1  # Heure de bureau
+        elif (x in range(6, 9)) or (x in range(16, 20)):
+            return 2  # Heure de pointe
+        elif (x in range(20, 24)) or (x in range(0, 6)):
+            return 3  # Nuit
+        else:
+            return -1
 
-    # Sauvegarde du DataFrame transformé
-    save_transformed_dataset(transformed_df, "data/processed/transformed_dataset.csv")
+    df['hour_cat'] = df['hour_cat'].apply(reduce_categories_hour)
+
+    # 2. Imputation des valeurs manquantes (-1) par le mode le plus fréquent de chaque colonne
+    imputer = SimpleImputer(strategy='most_frequent')
+    df = pd.DataFrame(imputer.fit_transform(df.replace(-1, np.nan)), columns=df.columns)
+
+    # 3. Calcul de l'âge de l'usager au moment de l'accident
+    df['age_usag'] = df['an'] - df['an_nais']
+
+    # 4. Catégorisation de l'âge à l'aide d'un transformateur personnalisé
+    class AgeCat(BaseEstimator, TransformerMixin):
+        def fit(self, X, y=None):
+            return self
+
+        def transform(self, X):
+            return pd.DataFrame(pd.cut(X['age_usag'], 
+                                       bins=[0, 12, 18, 30, 58, np.max(X['age_usag'])],
+                                       labels=['Enfant', 'Adolescent', 'Jeune adulte', 'Adulte', 'Senior']))
+
+    # Appliquer le transformateur personnalisé pour catégoriser l'âge
+    age_categorized = AgeCat()
+    df['age_category'] = age_categorized.fit_transform(df[['age_usag']])
+
+    # 5. Ajouter une colonne pour le jour de la semaine
+    def jour_de_la_semaine(date):
+        jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+        return jours[date.weekday()]
+
+    # Convertir la colonne 'hrmn' pour s'assurer que toutes les heures sont au format HH:MM
+    df["hrmn"] = df["hrmn"].apply(lambda x: str(x).zfill(4))
+
+    # Création d'une colonne datetime
+    df["datetime_str"] = (
+        df["an"].astype(str) + "-" + df["mois"].astype(str) + "-" + df["jour"].astype(str) + " " + df["hrmn"]
+    )
+    df["datetime"] = pd.to_datetime(df["datetime_str"], format="%Y-%m-%d %H:%M")
+    df["jour_sem"] = df["datetime"].apply(jour_de_la_semaine)
+
+    # 6. Encodage des variables catégorielles avec LabelEncoder
+    # Encodage de la catégorie d'âge
+    label_encoder_age_category = LabelEncoder()
+    df['age_category_encoded'] = label_encoder_age_category.fit_transform(df['age_category'])
+
+    # Encodage du jour de la semaine
+    label_encoder_jour_sem = LabelEncoder()
+    df['jour_sem_encoded'] = label_encoder_jour_sem.fit_transform(df['jour_sem'])
+
+    # 7. Nettoyage des colonnes inutiles
+    # Suppression des variables "temps" et autres colonnes utilisées temporairement
+    df.drop(columns=['an', 'an_nais', 'jour', 'mois', 'hrmn', 'age_usag', 
+                     'datetime_str', 'datetime', 'age_category', 'jour_sem'], inplace=True)
+
+    # 8. Assurez-vous que toutes les colonnes 'object' sont converties en 'int'
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = df[col].astype('int')
+
+    # 9. Suppression des doublons
+    df = df.drop_duplicates()
+
+    return df
+
+def select_variables_and_one_hot(df):
+    """
+    Sélection des variables et OneHot Encoding. On réalise également un undersampling car les données sont déséquilibrées.
+    """
+    y = df['grav']  # Cible
+
+    # Liste des variables à conserver = 15 variables - Pourrait être automatisé avec un SelectKBest éventuellement
+    variables_a_garder = ['catu', 'catv', 'obsm', 'place', 'manv', 'situ', 'agg', 'plan', 'age_category_encoded', 'int', 'sexe', 'lum', 'hour_cat', 'catr', 'choc']
+    # Garder seulement les colonnes spécifiées
+    df_15 = df.filter(variables_a_garder)
+    df_15 = df_15.astype('object')
+    X = pd.get_dummies(df_15, drop_first = True)
+
+    # Les classes sont déséquilibrées, ont réalise donc un undersampling
+    # Appliquer l'undersampling sur X et y en spécifiant un random_state
+    undersample = RandomUnderSampler(random_state=42)
+    X_resampled, y_resampled = undersample.fit_resample(X, y)
+
+    # Diviser les données en ensembles d'entraînement et de test
+    X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42, stratify=y_resampled)
+
+    return X_train, X_test, y_train, y_test
+
+def save_test_train(X_train, X_test, y_train, y_test):
+    """
+    Concatène les dataframes de train et test obtenus afin de les sauvegarder dans data/processed
+    """
+    datasets = {'train': (X_train, y_train), 'test': (X_test, y_test)}
+
+    for dataset_name, (X, y) in datasets.items():
+        # Convertir X et y en DataFrames pandas
+        X_df = pd.DataFrame(X)
+        y_df = pd.DataFrame(y)
+
+        # Concaténer X et y en un seul DataFrame
+        dataset_df = pd.concat([X_df, y_df], axis=1)
+        
+        # Exporter le DataFrame au format CSV
+        output_filename = os.path.join(PROCESSED_DATA_DIR, f"{dataset_name}_15variables_stratified.csv")
+        dataset_df.to_csv(output_filename, index=False)
