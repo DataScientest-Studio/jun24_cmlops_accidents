2046b546a884240c3d8198b43384e39f4c377d0c
Nico <nicolas.fenassile@gmail.com>
2024-10-22 16:40:42 +0200

    add file to test etl

---
+++ src/test_etl.py
@@ -0,0 +1,8 @@
+from etl import run_etl
+
+if __name__ == "__main__":
+    try:
+        run_etl()
+        print("ETL pipeline executed successfully.")
+    except Exception as e:
+        print(f"ETL pipeline failed with error: {e}")
