85bbbf11bb30fac7875d7361a5cd0fbdd78e8e2f
Nico <nicolas.fenassile@gmail.com>
2024-10-22 15:37:32 +0200

    Separation of ETl and model pipeline

--- src/etl.py
+++ src/etl.py
@@ -9,7 +9,7 @@ def run_etl():
     """
     Exécute la pipeline ETL complète.
     """
-    # S'ssurer que le dossier de sortie existe
+    # S'assurer que le dossier de sortie existe
     os.makedirs(PROCESSED_DATA_DIR, exist_ok=True)
     
     # Étape 1: Extraire et fusionner les données
@@ -44,38 +44,3 @@ def run_etl():
     print("Sauvegarde du DataFrame transformé...")
     save_final_dataset(features_df, features_output_path)
     print(f"Le fichier transformé a été sauvegardé à {features_output_path}")
-
-    # Étape 7: Sélection des variables, OneHotEncoding et Undersampling
-    print("Sélection des variables, OneHotEncoding et Undersampling...")
-    encoded_df = select_variables_and_one_hot(features_df)
-    print("Données encodées")
-
-    # Étape 8: Sauvegarder les dataframes train et test encodés
-    print("Sauvegarde des dataframes encodés...")
-    save_test_train(encoded_df)
-    print(f"Les dataframes test et train ont été sauvegardé")
-
-    # Étape 9: Charger le df d'entrainement pour entrainer le modèle
-    train_path = os.path.join(PROCESSED_DATA_DIR, 'train_15variables_stratified.csv')
-    print("Chargement des données d'entrainement...")
-    train_data = load_data(train_path)
-    print("Données d'entrainement chargées")
-
-    # Étape 10: Entrainer le modèle
-    print("Entrainement du modèle...")
-    model = train_model(train_data)
-    print("Modèle entrainé")
-
-    # Étape 11: Sauvegarder le modèle
-    print("Sauvegarde du modèle...")
-    save_model(model)
-
-    # Étape 12: Charger le df de test pour entrainer le modèle
-    test_path = os.path.join(PROCESSED_DATA_DIR, 'test_15variables_stratified.csv')
-    print("Chargement des données de test...")
-    test_data = load_data(test_path)
-    print("Données de test chargées")
-
-    # Étape 13: Evaluation du modèle sur les données de test
-    print("Evaluation du modèle...")
-    evaluate_model(test_data, model)
