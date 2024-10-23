dd115113ea27244b0e003938944a921e26ffd3a1
Nico <nicolas.fenassile@gmail.com>
2024-10-23 18:30:01 +0200

    added logger

--- src/etl.py
+++ src/etl.py
@@ -3,7 +3,20 @@ from features.build_features import transform_data, build_model_features, select
 from models.train_model import load_data, train_model, save_model
 from models.predict_model import evaluate_model
 from config import PROCESSED_DATA_DIR
+
 import os
+import logging
+
+# Configuration du logger
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("etl_pipeline.log"),  # Sauvegarde dans root, modifiable au besoin
+        logging.StreamHandler()  # Affichage dans la console
+    ]
+)
+logger = logging.getLogger(__name__)
 
 def run_etl():
     """
@@ -13,34 +26,34 @@ def run_etl():
     os.makedirs(PROCESSED_DATA_DIR, exist_ok=True)
     
     # Étape 1: Extraire et fusionner les données
-    print("Extraction et fusion des données...")
+    logger.info("Extraction et fusion des données...")
     df_final = merge_datasets()
-    print("Données extraites et fusionnées.")
+    logger.info("Données extraites et fusionnées.")
     
     # Étape 2: Sauvegarder le DataFrame final dans data/processed
     final_output_path = os.path.join(PROCESSED_DATA_DIR, 'df_final.csv')
-    print("Sauvegarde du DataFrame concaténé et homogénéisé...")
+    logger.info("Sauvegarde du DataFrame concaténé et homogénéisé...")
     save_final_dataset(df_final, final_output_path)
-    print(f"Le fichier concaténé et homogénéisé a été sauvegardé à {final_output_path}")
+    logger.info(f"Le fichier concaténé et homogénéisé a été sauvegardé à {final_output_path}")
 
     # Étape 3: Transformation des données
-    print("Transformation des données...")
+    logger.info("Transformation des données...")
     transformed_df = transform_data(df_final)
-    print("Données transformées.")
+    logger.info("Données transformées.")
 
     # Étape 4: Sauvegarder le DataFrame transformé dans data/processed
     transformed_output_path = os.path.join(PROCESSED_DATA_DIR, 'df_transformed.csv')
-    print("Sauvegarde du DataFrame transformé...")
+    logger.info("Sauvegarde du DataFrame transformé...")
     save_final_dataset(transformed_df, transformed_output_path)
-    print(f"Le fichier transformé a été sauvegardé à {transformed_output_path}")
+    logger.info(f"Le fichier transformé a été sauvegardé à {transformed_output_path}")
 
     # Étape 5: Création des features du modèle
-    print("Création des features du modèle...")
+    logger.info("Création des features du modèle...")
     features_df = build_model_features(transformed_df)
-    print("Features créées")
+    logger.info("Features créées")
 
     # Étape 6: Sauvegarder le DataFrame avec features dans data/processed
     features_output_path = os.path.join(PROCESSED_DATA_DIR, 'df_with_features.csv')
-    print("Sauvegarde du DataFrame transformé...")
+    logger.info("Sauvegarde du DataFrame transformé...")
     save_final_dataset(features_df, features_output_path)
-    print(f"Le fichier transformé a été sauvegardé à {features_output_path}")
+    logger.info(f"Le fichier transformé a été sauvegardé à {features_output_path}")
