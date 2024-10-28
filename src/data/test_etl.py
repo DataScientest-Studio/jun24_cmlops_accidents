b5aa433a1e8a3572969b5c591f8a699977678e4b
Gaetan GAUTHIER <gaetan.gauthier@me.com>
2024-10-28 14:52:36 +0100

    Suppression du fichier test_etl

--- src/data/test_etl.py
+++
@@ -1,8 +0,0 @@
-from data.etl import run_etl
-
-if __name__ == "__main__":
-    try:
-        run_etl()
-        print("ETL pipeline executed successfully.")
-    except Exception as e:
-        print(f"ETL pipeline failed with error: {e}")
